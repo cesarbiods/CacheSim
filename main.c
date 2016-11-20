a7b99e1bb55149efcfbf90be89e949cf3f85badb
cesar <crincon@oswego.edu>
2016-11-19 21:00:27 -0500

    masks and allocate
    allocated the 2d array and set the masks, and it runs

--- main.c
+++ main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include <time.h>
 
-struct Line{
-    int tag;
+int cycles;
+
+typedef struct {
+    long tag;
+    bool valid;
     int index;
-    int offset;
-} ;
+    int accesses;
+} line;
 
-struct Line** generateCache(int sets, int lines) {
-    struct Line** cache = malloc(sizeof(struct Line) * (lines * sets));
-    return cache;
-}
-
-void killCache(struct Line** cache) {
+void killCache(line** cache) {
     free(*cache);
     free(cache);
 }
 
 int main() {
+
     int setNum;
     int lineNum;
     int blockSize;
@@ -26,16 +28,51 @@ int main() {
     char rp[3];
     int hTime;
     int mPenalty;
+    int s;
+    int b;
+    line** cache;
+
+    clock_t start, end, total;
+    start = clock();
+    printf("%ld\n", start);
 
     printf("What kind of cache are we dealing with here?\n");
     scanf("%d", &setNum);
+    s = log2(setNum);
     scanf("%d", &lineNum);
     scanf("%d", &blockSize);
+    b = log2(blockSize);
     scanf("%d", &addrBitsNum);
     scanf("%s", rp);
     scanf("%d", &hTime);
     scanf("%d", &mPenalty);
+
+    cache =  malloc(sizeof(line *) * setNum);
+    for (int i = 0; i < setNum; i++) {
+        cache[i] = malloc(sizeof(line *) * lineNum);
+    }
+    for (int i = 0; i < setNum; i++) {
+        for (int j = 0; j < lineNum; j++) {
+            cache[i][j].tag = 0;
+            cache[i][j].valid = false;
+            cache[i][j].index = 0;
+            cache[i][j].accesses = 0;
+        }
+    }
+
+    long bMask = ~(~0 << b);
+    long sMask = ~(~0 << s) << b;
+    long tMask = ~0 << (s + b);
+
+    end = clock();
+    printf("%ld\n", end);
+
     printf("Please enter your addresses in hex form, or -1 to quit\n");
+    long address = 0;
+    while(address != -1) {
+        scanf("%lu", &address);
+    }
     
+    killCache(cache);
     return 0;
 }
