0c175dfefddac6db5468ff580591ce3c55491afc
cesar <crincon@oswego.edu>
2016-11-20 12:25:10 -0500

    implemented LFU and LRU checks

--- main.c
+++ main.c
@@ -5,8 +5,6 @@
 #include <stdbool.h>
 #include <time.h>
 
-int cycles;
-
 typedef struct {
     long tag;
     bool valid;
@@ -23,13 +21,14 @@ void killCache(line** cache) {
 bool check_hit(long address, line* l, long tagMask) {
   bool ret_value = (l->valid && (l->tag == (address & tagMask)));
   if (ret_value) {
-    l->accesses++;
+    l->accesses++; //does this really go here?
     l->time = clock();
   }
   return ret_value;
 }
 
 int main() {
+    long start = clock();
 
     int setNum;
     int lineNum;
@@ -41,6 +40,7 @@ int main() {
     int s;
     int b;
     line** cache;
+    int accNumber;
 
     printf("What kind of cache are we dealing with here?\n");
     scanf("%d", &setNum);
@@ -70,11 +70,73 @@ int main() {
     long sMask = ~(~0 << s) << b;
     long tMask = ~0 << (s + b);
 
+    long test;
+    //scanf("0x%lu", &test);
+    //printf("%lu", test);
+    if (check_hit(test, &cache[0][0], tMask)){
+        printf("true");
+    } else {
+        printf("false");
+    }
+
     printf("Please enter your addresses in hex form, or -1 to quit\n");
     long address = 0;
+
     while(address != -1) {
         scanf("%lu", &address);
+        bool hit = false;
+        for (int i = 0; i < lineNum; i++) {
+            if (check_hit(address, &cache[(address & sMask) >> b][i], tMask)) {
+                hit = true;
+                break;
+            }
+        }
+        if (hit) {
+            accNumber++;
+
+        }
+        else {
+            accNumber++;
+            bool done = false;
+            while (!done) {
+                for (int i = 0; i < lineNum; i++) {
+                    if (!cache[(address & sMask) >> b][i].valid) {
+                        //TODO take address and put it in the line
+                        done = true;
+                        break;
+                    }
+                }
+                if (strcmp(rp, "LFU") != 0) {
+                    int temp = 100000;
+                    for (int i = 0; i < lineNum; i++) {
+                        if (!(temp < cache[(address & sMask) >> b][i].accesses)) {
+                            temp = cache[(address & sMask) >> b][i].accesses;
+                        }
+                    }
+                    for (int i = 0; i < lineNum; i++) {
+                        if (temp == cache[(address & sMask) >> b][i].accesses) {
+                            //TODO replace the line with the new address
+                            break;
+                        }
+                    }
+                } else {
+                    long temp = 99999999999;
+                    for (int i = 0; i < lineNum; i++) {
+                        if (!(temp < cache[(address & sMask) >> b][i].time)) {
+                            temp = cache[(address & sMask) >> b][i].time;
+                        }
+                    }
+                    for (int i = 0; i < lineNum; i++) {
+                        if (temp == cache[(address & sMask) >> b][i].time) {
+                            //TODO replace the line with the new address
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
+    long end = clock();
     
     killCache(cache);
     return 0;
